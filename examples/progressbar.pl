cbac19c97c8a7b530f1fa3b8c311d2e7950753cb
kmx <kmx@cpan.org>
2010-09-22 18:05:21 +0200

    fixed progressbar demo

--- examples/progressbar.pl
+++ examples/progressbar.pl
@@ -4,20 +4,22 @@ use warnings;
 use IUP;
 
 my $progressbar = IUP::ProgressBar->new();
-#$progressbar->SHOW_TEXT("YES");
 
 sub idle_cb {
   my $value = $progressbar->VALUE;
-  $value += 0.0001;
-  $value = 0.0 if ( $value > 1.0 );
+  if ( $value == 1 ) {
+   $value = 0.0001;
+  }
+  else {
+   $value += 0.0001;
+  }
   $progressbar->VALUE($value);
   return IUP_DEFAULT;
 }
 
 my $dlg = IUP::Dialog->new( child=>$progressbar, TITLE=>"IupProgressBar");
-#$dlg->SIZE("QUARTERxEIGHTH");
+# xxx $dlg->SIZE("QUARTERxEIGHTH");
 
-# xxx TODO SetIdle not implemented yet
 # Registers idle callback;
 IUP->SetIdle(\&idle_cb);
 
