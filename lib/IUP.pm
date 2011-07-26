ba4a065db6403ed28cb1f5acb2505693e302d04c
kmx <kmx@cpan.org>
2011-07-26 12:10:29 +0200

    v0.001_16

--- lib/IUP.pm
+++ lib/IUP.pm
@@ -10,7 +10,7 @@ use IUP::Internal::LibraryIup;
 use IUP::Constants;
 
 # following recommendation from http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.001_15";
+our $VERSION = "0.001_16";
 $VERSION = eval $VERSION;
 
 sub BEGIN {
--- lib/IUP/Normalizer.pm
+++ lib/IUP/Normalizer.pm
@@ -3,7 +3,6 @@ use strict;
 use warnings;
 use base 'IUP::Internal::Element';
 use IUP::Internal::LibraryIup;
-use Carp;
 
 sub _create_element {
   return shift->_proc_child_param(\&IUP::Internal::LibraryIup::_IupNormalizer, @_);
