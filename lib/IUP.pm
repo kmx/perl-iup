b52f44cda1411ff902fea9d708a24555f4f44a6f
kmx <kmx@cpan.org>
2012-07-15 21:58:18 +0200

    version bump + TODO update

--- lib/IUP.pm
+++ lib/IUP.pm
@@ -9,7 +9,7 @@ use IUP::Internal::LibraryIup;
 use IUP::Constants;
 
 # following recommendation from http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/
-our $VERSION = "0.004";
+our $VERSION = "0.005_1";
 $VERSION = eval $VERSION;
 
 sub BEGIN {
